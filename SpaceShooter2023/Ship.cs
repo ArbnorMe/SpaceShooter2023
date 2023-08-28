db81dd08b57ee53d67fdda3c41e32fbc74da7434
Arbnor Memedi <arbnormemedi@outlook.de>
2023-08-28 12:50:48 +0200

    Projektdateien hinzufügen.

---
+++ SpaceShooter2023/Ship.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceShooter2023
+{
+    public class Ship : SpaceObject
+    {
+    }
+}
---
+++ SpaceShooter2023/SpaceObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceShooter2023
+{
+    public class SpaceObject
+    {
+        public int X_Position
+        { get; set; }
+
+        public int Y_Position
+        { get; set; }
+    }
+}
